fc43221d6f1e9d5890f103effaf1f3ce27ac044d
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-18 22:42:49 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double tamanhoBase, altura, area;
+
+Console.Write("Insira o tamanho da base do triângulo: ");
+tamanhoBase = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Insira a altura do triângulo: ");
+altura = Convert.ToDouble(Console.ReadLine()!);
+
+area = (altura * tamanhoBase) / 2;
+
+Console.WriteLine($"A área desse triângulo é de {area}");
